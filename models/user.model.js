009a6125702de045bb97bc3e4e9c439bce43ed48
Muhammad Subhan Khan <subhanism@outlook.com>
2023-04-09 22:25:53 +0500

    added roles in user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,42 +1,46 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const { Schema } = mongoose
+const { Schema } = mongoose;
 
 const userSchema = new Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            select: false,
-        },
-        name: {
-            type: Schema.Types.ObjectId,
-            ref: 'Member',
-        },
-        admin_roles: [
-            {
-                type: String,
-                required: true,
-            },
-        ],
-        access_description: {
-            type: String,
-        },
-        dateCreated: {
-            type: Date,
-            required: true,
-            default: Date.now,
-        },
+  {
+    email: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
-)
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "subAdmin", "user"],
+    },
+    name: {
+      type: Schema.Types.ObjectId,
+      ref: "Member",
+    },
+    admin_roles: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    access_description: {
+      type: String,
+    },
+    dateCreated: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
